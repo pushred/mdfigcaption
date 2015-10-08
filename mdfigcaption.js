328a53e2b32987dcc30bf4656debebde6636e5cf
Eric Lanehart <pushred@gmail.com>
2015-10-08 10:01:48 -0500

    Don't spam console

--- mdfigcaption.js
+++ mdfigcaption.js
@@ -5,7 +5,6 @@ var MdFigCaption = Plugin(
     function(match, utils) {
         var caption = match[1];
         var url = match[2];
-        console.log('img stuff: ', caption, url, match);
         
         //TODO: is there a safer way to do this? I unno...
         var html = '<figure><img src="' + url + '" /><figcaption>' + caption + '</figcaption></figure>';
